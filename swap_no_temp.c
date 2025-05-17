a7b573bb6f4854dcd68392165259087feda1a673
soufy14 <soufiyacouba@gmail.com>
2025-05-17 04:24:40 +0100

    bash

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,14 +2,14 @@
 
 int main() {
 
-    int a = 0, b = 0;
+    int a, b;
+
+   scanf("%d %d", &a, &b); // lire entre de l'utilisateur
 // Inversion sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
-scanf("%d %d", &a, &b); // lire entre de l'utilisateur
-
-printf("Après échange: a = %d, b = %d\n", a, b);
-    return 0;
+   printf("Après échange: a = %d, b = %d\n", a, b);
+return 0;
 }
 // céest un programme qui lit deux entiers et les échange sans utiliser de variable temporaire 
