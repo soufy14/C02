05590c3d1cd07086442a66808dd55e8a51a7029b
soufy14 <soufiyacouba@gmail.com>
2025-05-17 02:08:13 +0100

    bash

---
+++ sum_two_integers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0, b = 0;
+    int resultat = 0;
+
+    scanf("%d %d", &a, &b); 
+    
+    resultat = a + b;
+    printf("La somme est : %d\n", resultat);
+    return 0;
+}
+// c'est un programme en C qui lit deux entiers et affiche leur somme 
