f3627ca2e29079c5e02424f84a206516e6fcd005
soufy14 <soufiyacouba@gmail.com>
2025-05-17 02:53:36 +0100

    bash

---
+++ even_or_odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int nombre = 0;
+
+    scanf("%d", &nombre); // lire entre de l'utilisateur 
+// la condition qui permet de diviser par  2
+    if ( nombre % 2 == 0 ) {
+    
+    printf("%d est pair\n", nombre);
+    } else {
+        
+        printf("%d est impair\n", nombre);
+    }
+    return 0; 
+}
+// c'est un programme qui lit un nombre entier et affiche s'il est pair ou impair 
